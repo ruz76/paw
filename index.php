8990bdfb45b40d617409252c834fb8d9237862bf
ruz76 <jan.ruzicka.vsb@gmail.com>
2016-10-05 09:53:01 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="">
+  <head>  
+  
+    <title>PAW</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="robots" content="index, follow">
+	<meta name="revisit-after" content="7 days">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	        
+</head>
+<body>
+<?php
+	echo "Ahoj Karle éíščížáíáčžéášč";
+	for ($i=0; $i<10; $i++) {
+		echo $i." hhhhhh<br/>";
+	}
+?>
+</body>
+</html>
